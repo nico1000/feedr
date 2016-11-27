5a63a4eadaf5488074bc7a319ec1ee8860ddf1d1
nico <nico@potz1000.ch>
2016-11-27 23:15:12 +0100

    Remove test code

--- js/Chords.js
+++ js/Chords.js
@@ -2,22 +2,5 @@ export class Chords {
   constructor() {
   }
   
-  // render all chords to dom elements
-  static get allChords() {
-    return {
-      'C': {
-        positions: '577655',
-        fingers: '134211',
-      },
-      'Am': {
-        positions: '577655',
-        fingers: '134211',
-      },
-    };
-  }
-
-  static renderChords() {
-    return this.allChords;
-  }
 
 }

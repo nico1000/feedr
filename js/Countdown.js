c3f1e4abcb8113a7237dd9da15616ec68485e594
Nico Bernold <nb@feinheit.ch>
2016-12-30 09:38:09 +0100

    Split scss files

--- js/Countdown.js
+++ js/Countdown.js
@@ -11,25 +11,37 @@ export default class Countdown extends React.Component {
     };
   }
   render() {
-      return (
-        <div className="countdown">
-          <div className="countdown__chords" >
-            <Chord chordName={ this.props.chord1 } />
-            <Chord chordName={ this.props.chord2 } />
-          </div>
-          <div className="countdown__info">
-            { this.state.running ? (
-              <span className="countdown__time">{ Math.abs(this.state.time) }</span>
-            ) : (
-              <span className="countdown__finished">fertig</span>
-            )}
-          </div>
+
+    const countdownInfo = this.state.running ? (
+      <div className="countdown__info">
+        <span className="countdown__time">{ Math.abs(this.state.time) }</span>
+      </div>
+    ) : (
+      <div className="countdown__info">
+        <input type="number" className="countdown__result" />
+        <div className="countdown__icon countdown__icon--save">
+          <i className="fa fa-floppy-o" />
         </div>
-      );
+        <div className="countdown__icon countdown__icon--delete">
+          <i className="fa fa-trash" />
+        </div>
+      </div>
+    );
+
+    return (
+      <div className="countdown">
+        <div className="countdown__chords" >
+          <Chord chordName={ this.props.chord1 } />
+          <Chord chordName={ this.props.chord2 } />
+        </div>
+        { countdownInfo }
+      </div>
+    );
   }
 
   componentDidMount() {
     this.interval = setInterval(this.updateTime, 1000);
+    $('.countdown').classList.add('countdown--start-animation');
   }
 
   componentWillUnmount() {
